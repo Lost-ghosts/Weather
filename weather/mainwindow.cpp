b5dd024806135acc3476e8d3a8aab0927c15532a
Lost-ghosts <56260326+Lost-ghosts@users.noreply.github.com>
2021-09-07 18:52:17 +0800

    First

---
+++ weather/mainwindow.cpp
@@ -0,0 +1,48 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    //设置窗口属性为没有边框
+    setWindowFlag(Qt::FramelessWindowHint);
+    //设置窗口固定大小
+    setFixedSize(width(),height());
+    //右键退出
+    exitMenu = new QMenu(this);
+    exitAct = new QAction;
+    exitAct->setText("exit");
+    exitAct->setIcon(QIcon(":/weatherIco/close.ico"));
+    connect(exitAct,SIGNAL(triggered(bool)),this,SLOT(slot_exitApp()));
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::mouseMoveEvent(QMouseEvent *event)
+{
+    move(event->globalPos()-mPos);
+}
+
+void MainWindow::mousePressEvent(QMouseEvent *event)
+{
+    mPos = event->globalPos()-this->pos();
+}
+
+void MainWindow::contextMenuEvent(QContextMenuEvent *event)
+{
+    exitMenu->exec(QCursor::pos());
+    event->accept();
+
+}
+
+void MainWindow::slot_exitApp()
+{
+    qApp->exit(0);
+}
+
